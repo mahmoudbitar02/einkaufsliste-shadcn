9cf37666b908fd4783f44d504e9f5530e2e5dd89
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-08-28 22:31:33 +0200

    finish first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import { Button } from "@/components/ui/button";
-import { Slider } from "@/components/ui/slider";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
   return (
-    <>
-      <h1 className="bg-slate-900">hallo</h1>
-      <Button variant="outline">Button</Button>
-      <Slider defaultValue={[75]} max={100} step={1} className="mx-auto w-full max-w-xs" />
-    </>
+    <div className="max-w-lg mx-auto flex flex-col justify-center items-center mt-20 ">
+      <h1 className="text-3xl font-semibold">Einkaufsliste</h1>
+      <div className="flex w-full gap-2 mt-10">
+        <Input className="" placeholder="Product eingeben..." />
+        <Input className="w-14" type="number" min={1} />
+      </div>
+      <Button disabled className="mt-3 w-full">
+        Eintrag Hinzufügen
+      </Button>
+
+      <div className="flex w-full justify-between items-center flex-col mt-7 gap-2">
+        <div className="flex w-full justify-between items-center border rounded-2xl p-7 text-card-foreground shadow">
+          <div className="flex flex-col justify-center items-center gap-1 ">
+            <h3>product</h3>
+            <p>Anzahl: 1</p>
+          </div>
+          <Button>Abhaken</Button>
+        </div>
+
+        <div className="flex w-full justify-between items-center border rounded-2xl p-7 text-card-foreground shadow">
+          <div className="flex flex-col justify-center items-center gap-1 ">
+            <h3>product</h3>
+            <p>Anzahl: 1</p>
+          </div>
+          <Button>Abhaken</Button>
+        </div>
+        <div className="flex w-full justify-between items-center border rounded-2xl p-7 text-card-foreground shadow">
+          <div className="flex flex-col justify-center items-center gap-1 ">
+            <h3>product</h3>
+            <p>Anzahl: 1</p>
+          </div>
+          <Button>Abhaken</Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
